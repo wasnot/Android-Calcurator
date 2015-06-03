8f14fe77dd30c2f9e7f6012fb1d85602ae30c760
wasnot <a.aida@freebit.net>
2015-06-03 19:27:13 +0900

    replaced BigDecimal for correct calculation

--- app/src/main/java/net/wasnot/android/calculator/MainActivity.java
+++ app/src/main/java/net/wasnot/android/calculator/MainActivity.java
@@ -1,15 +1,18 @@
 
 package net.wasnot.android.calculator;
 
-import net.wasnot.android.calculator.util.LogUtil;
+import java.math.BigDecimal;
 
+import net.wasnot.android.calculator.util.LogUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
@@ -28,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     public TextView newValueText;
     @InjectView(R.id.actionText)
     public TextView actionText;
+    @InjectView(R.id.gridLayout)
+    public View gridLayout;
 
     private StringBuilder mCurrValue = new StringBuilder();
     private StringBuilder mNewValue = new StringBuilder();
@@ -40,6 +45,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.inject(this);
+
+        Display dsp = getWindowManager().getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        dsp.getMetrics(metrics);
+
+        gridLayout.setMinimumHeight((int) (metrics.widthPixels * 5 / 4f));
     }
 
     @Override
@@ -83,9 +94,24 @@ public class MainActivity extends AppCompatActivity {
             case R.id.button7:
             case R.id.button8:
             case R.id.button9:
-                mNewValue.append(t.getText());
+                // アクションがないときは前回値はリセット
+                if (mAction <= 0)
+                    mCurrValue.setLength(0);
+                // 16桁を上限とする
+                if (mNewValue.length() < 16) {
+                    // 小数点を含まず0から始まるときは0を除く
+                    if (mNewValue.length() > 0 && mNewValue.indexOf(".") < 0
+                            && '0' == mNewValue.charAt(0))
+                        mNewValue.deleteCharAt(0);
+                    mNewValue.append(t.getText());
+                } else {
+                    Toast.makeText(this, "これ以上入力できません", Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.buttonPoint:
+                // アクションがないときは前回値はリセット
+                if (mAction <= 0)
+                    mCurrValue.setLength(0);
                 if (mNewValue.indexOf(".") < 0) {
                     if (mNewValue.length() == 0) {
                         mNewValue.append("0.");
@@ -95,6 +121,13 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             case R.id.buttonPlusMinus:
+                if (mNewValue.length() > 0) {
+                    int indexMinus = mNewValue.indexOf("-");
+                    if (indexMinus == -1)
+                        mNewValue.insert(0, "-");
+                    else
+                        mNewValue.deleteCharAt(indexMinus);
+                }
                 break;
             case R.id.buttonAdd:
             case R.id.buttonSubtract:
@@ -119,8 +152,8 @@ public class MainActivity extends AppCompatActivity {
             case R.id.buttonEqual:
                 // どちらもある時は計算結果を既存値へ、新規値はクリア、アクションもクリア
                 if (mCurrValue.length() > 0 && mNewValue.length() > 0) {
-                    calculate();
-                    mAction = -1;
+                    if (calculate())
+                        mAction = -1;
                 }
                 break;
             case R.id.buttonClear:
@@ -149,10 +182,10 @@ public class MainActivity extends AppCompatActivity {
                 actionText.setText("-");
                 break;
             case R.id.buttonMultiply:
-                actionText.setText("*");
+                actionText.setText("×");
                 break;
             case R.id.buttonDivide:
-                actionText.setText("/");
+                actionText.setText("÷");
                 break;
             default:
                 actionText.setText(null);
@@ -160,49 +193,68 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void calculate() {
-        double curr = Double.parseDouble(mCurrValue.toString());
-        double newv = Double.parseDouble(mNewValue.toString());
-        double next;
+    /**
+     * @return success
+     */
+    private boolean calculate() {
+        BigDecimal currValue = new BigDecimal(mCurrValue.toString());
+        BigDecimal newValue = new BigDecimal(mNewValue.toString());
+        BigDecimal next;
         String nextValue = "";
         // アクションがある時のみ計算。
         if (mAction > 0) {
             switch (mAction) {
                 case R.id.buttonAdd:
-                    next = curr + newv;
-                    nextValue = Double.toString(next);
+                    next = currValue.add(newValue);
+                    nextValue = next.toPlainString();
                     break;
                 case R.id.buttonSubtract:
-                    next = curr - newv;
-                    nextValue = Double.toString(next);
+                    next = currValue.subtract(newValue);
+                    nextValue = next.toPlainString();
                     break;
                 case R.id.buttonMultiply:
-                    next = curr * newv;
-                    nextValue = Double.toString(next);
+                    next = currValue.multiply(newValue);
+                    nextValue = next.toPlainString();
                     break;
                 case R.id.buttonDivide:
-                    if (newv != 0) {
-                        next = curr / newv;
-                        nextValue = Double.toString(next);
+                    if (!newValue.equals(BigDecimal.ZERO)) {
+                        try {
+                            // TODO 事前に循環少数か判別できないか。
+                            next = currValue.divide(newValue);
+                        } catch (ArithmeticException e) {
+                            // 循環小数は10桁で丸める
+                            next = currValue.divide(newValue, 10, BigDecimal.ROUND_HALF_UP);
+                        }
+                        nextValue = next.toPlainString();
+                    } else {
+                        Toast.makeText(this, "0で割ることはできません", Toast.LENGTH_SHORT).show();
+                        return false;
                     }
                     break;
+            }
+            // 計算結果が20桁以上になる場合は桁あふれの警告を出し、計算しない
+            if (nextValue.length() > 20) {
+                Toast.makeText(this, "計算結果の桁数が多すぎます", Toast.LENGTH_SHORT).show();
+                return false;
             }
             mCurrValue.setLength(0);
             mNewValue.setLength(0);
             mCurrValue.append(nextValue);
-
+            return true;
             // Obtain a Realm instance
-//            Realm realm = Realm.getInstance(this);
-//            realm.beginTransaction();
-//            // Create a new object
-//            CalcurateResult result = realm.createObject(CalcurateResult.class);
-//            // result.setName("Wasabeef");
-//            // result.setEmail("chip@wasabeef.jp");
-//            realm.commitTransaction();
+            // Realm realm = Realm.getInstance(this);
+            // realm.beginTransaction();
+            // // Create a new object
+            // CalcurateResult result =
+            // realm.createObject(CalcurateResult.class);
+            // // result.setName("Wasabeef");
+            // // result.setEmail("chip@wasabeef.jp");
+            // realm.commitTransaction();
 
         }
         // アクションがない時は？
         else {
+            return false;
         }
     }
 }
